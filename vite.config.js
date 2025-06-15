fff648c74fca39d7a9ce44a3421b3e2389f1d87b
SekiKentaro <lily.bloom.world@gmail.com>
2025-06-15 14:11:18 +0900

    fix vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // Vite に React プラグインを読み込ませるだけの設定
 export default defineConfig({
-  base: 'mokomoko-pastel',
+  base: '/mokomoko-pastel/',
   plugins: [react()]
 });
